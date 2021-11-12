82964d14d0e6df21b970eca5662c827b9b5d42fd
Ravidu Yasas <88565232+raviyasa@users.noreply.github.com>
2021-11-13 00:31:36 +0845

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,16 +1,17 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/{}'>Mᴏᴠɪᴇs Fɪɴᴅᴇʀ 😊</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍
+    START_TXT = """Hᴀɪɪ {} 🖖🤟
+Mʏ Nᴀᴍᴇ Is <a href='https://t.me/{}'>Mᴏᴠɪᴇs Fɪɴᴅᴇʀ 😊</a>.... I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇs Iɴ Bᴜᴛᴛᴏɴs. Jᴜsᴛ Aᴅᴅ Mᴇ Tᴏ Yᴏᴜʀ Gʀᴏᴜᴘ Aɴᴅ Sᴇᴇ Tʜᴇ Mᴀᴊɪᴄ. 😛
 ©️ Powered By <a href="https://t.me/media_bot_updates">Mᴇᴅɪᴀ Bᴏᴛ</a>"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: Mᴏᴠɪᴇs Fɪɴᴅᴇʀ 😊
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href="https://t.me/darkz_hacker_devil">Ravidu Yasas</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    HELP_TXT = """Hᴇʏ {}
+Aʀᴇ Yᴏᴜ Hᴀᴠᴇ Aɴʏ Pʀᴏʙʟᴇᴍ. Hᴇʀᴇ Is Tʜᴇ Hᴇʟᴘ Fᴏʀ Mʏ Cᴏᴍᴍᴀɴᴅs.☺️😛"""
+    ABOUT_TXT = """☑️ 𝙼𝚈 𝙽𝙰𝙼𝙴: Mᴏᴠɪᴇs Fɪɴᴅᴇʀ 😊
+☑️ Cʀᴇᴀᴛᴏʀ : 🧑‍💻<a href="https://t.me/darkz_hacker_devil">Rᴀᴠɪᴅᴜ Yᴀsᴀs</a>
+☑️ Lɪʙʀᴀʀʏ : Pʏʀᴏɢʀᴀᴍ
+☑️ Lᴀɴɢᴜᴀɢᴇ : Pʏᴛʜᴏɴ 3
+☑️ Dᴀᴛᴀ Bᴀsᴇ : Mᴏɴɢᴏ DB
+☑️ Bᴏᴛ Sᴇʀᴠᴇʀ : Hᴇʀᴏᴋᴜ
+☑️ Bᴜɪʟᴅ Sᴛᴀᴛᴜs : v1.0.1 [ Bᴇᴛᴀ ]
+☑️ Sᴏᴜʀᴄᴇ Cᴏᴅᴇ : 🔐"""
     SOURCE_TXT = """<b>NOTE:</b>
 - Mᴏᴠɪᴇs Fɪɴᴅᴇʀ 😊 is a Locked project. 
 - Source - 🔐
